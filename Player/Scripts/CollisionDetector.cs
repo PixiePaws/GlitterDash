1491142b550a5f6193920ef911656ff752f489a0
Henri Hollanti <henri.hollanti@tuni.fi>
2025-05-28 13:22:58 +0300

    Fixed PlayerCharacter and TileMapLayer collision layers, added CollisionDetector into PlayerCharacter, added obstacle CircularSaw, added folders Art/Obstacles/ and Obstacles/

---
+++ Player/Scripts/CollisionDetector.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public partial class CollisionDetector : Area2D
+{
+    private bool _isPaused = false;
+    //private String _gameOverPath = "";
+    //private PackedScene _gameOverScene;
+
+    public override void _Ready()
+    {
+        //_gameOverScene = ResourceLoader.Load<PackedScene>(_gameOverPath);
+        BodyEntered += OnCollisionDetected;
+    }
+
+    public void OnCollisionDetected(Node node)
+    {
+        if (node.IsInGroup("Obstacles"))
+        {
+            GD.Print("Collided with obstacle!");
+            GD.Print("Game over!");
+            InstantiateGameOverScene();
+        }
+        
+    }
+
+    public void InstantiateGameOverScene()
+    {
+        GD.Print("InstantiateGameOverScene() was called");
+        /*if (_gameOverScene != null)
+        {
+            Node GameOverPanel = _gameOverScene.Instantiate();
+            AddChild(GameOverPanel);
+            GD.Print("Game over scene was instantiated");
+        }
+        else
+        {
+            GD.Print("Game over scene not found!");
+        }*/
+    }
+}
