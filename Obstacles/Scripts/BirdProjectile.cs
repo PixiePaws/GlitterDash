ab31d76accf36d0bb2b2961c6ee2fde86660ad43
Henri Hollanti <henri.hollanti@tuni.fi>
2025-07-23 11:19:44 +0300

    images for obstacles, created method for animating the BirdProjectile sprite, switched the sprite for BirdProjectile and CircularSaw

--- Obstacles/Scripts/BirdProjectile.cs
+++ Obstacles/Scripts/BirdProjectile.cs
@@ -7,16 +7,55 @@ namespace UnicornGame
     {
         [Export] private Vector2 _impulseVector = new Vector2(-50.0f, 0.0f);
         [Export] private Area2D _collisionDetector;
+        private string _spriteWingsUpPath;
+        private string _spriteWingsDownPath;
+        private Sprite2D _sprite;
+        private Texture2D _spriteWingsUp;
+        private Texture2D _spriteWingsDown;
         public override void _Ready()
         {
             //_impulseVector = new Vector2(-50.0f, 0.0f);
             _collisionDetector = GetNode<Area2D>("CollisionDetector");
             _collisionDetector.BodyEntered += OnCollisionDetected;
+            _sprite = GetNode<Sprite2D>("Sprite2D");
+            if (_sprite != null)
+            {
+                GD.Print("Succesfully got sprite reference in bird projectile");
+            }
+            GD.Print($"Bird projectile texture: {_sprite.Texture}");
+            _spriteWingsUpPath = "res://Art/Obstacles/Bird_WingUp.png";
+            _spriteWingsDownPath = "res://Art/Obstacles/Bird_WingDown.png";
+            _spriteWingsUp = ResourceLoader.Load<Texture2D>(_spriteWingsUpPath);
+            _spriteWingsDown = ResourceLoader.Load<Texture2D>(_spriteWingsDownPath);
+            _sprite.Texture = _spriteWingsUp;
             ApplyCentralImpulse(_impulseVector);
         }
+        public override void _Process(double delta)
+        {
+            AlternateSprite();
+        }
+
         public void OnCollisionDetected(Node node)
         {
             QueueFree();
+        }
+        public void AlternateSprite()
+        {
+            //GD.Print($"bird texture wings up {_sprite.Texture == _spriteWingsUp}");
+            //GD.Print($"bird texture wings down {_sprite.Texture == _spriteWingsDown}");
+            
+            if (_sprite.Texture == _spriteWingsUp)
+            {
+                _sprite.Texture = _spriteWingsDown;
+            }
+            else
+            {
+                _sprite.Texture = _spriteWingsUp;
+            }
+        }
+        public void CreateTimer()
+        {
+            Timer AlternatingTimer = new Timer();
         }
     }
 }
--- Obstacles/Scripts/CircularSaw.cs
+++ Obstacles/Scripts/CircularSaw.cs
@@ -6,18 +6,15 @@ namespace UnicornGame
 {
     public partial class CircularSaw : Sprite2D
     {
-        [Export] private float _rotationAmount = 2.0f;
+        [Export] private float _rotationAmount = 180.0f;
         [Export] private int _rotateSawCallAmount = 2000;
         public override void _Process(double delta)
         {
-            for (int i = 0; i < _rotateSawCallAmount; i++)
-            {
-                RotateSaw(delta);
-            }
+            RotateSaw(delta);
         }
         private void RotateSaw(double delta)
         {
-            RotationDegrees += _rotationAmount * (float)delta;
+            RotationDegrees += _rotationAmount * (float)delta * _rotateSawCallAmount;
         }
     }
 }
