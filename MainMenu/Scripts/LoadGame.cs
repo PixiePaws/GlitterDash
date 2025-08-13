5b58a58a2be46b3cb5994ab55947597538e3aabc
Henri Hollanti <henri.hollanti@tuni.fi>
2025-08-13 11:07:04 +0300

    Added method GetLastestTimeStampFromSave() into LoadGame.cs, made changes to SetButtonVisibilityAndText() in LoadGame.cs, time stamps are now working in LoadGame.tscn

--- MainMenu/Scripts/LoadGame.cs
+++ MainMenu/Scripts/LoadGame.cs
@@ -184,10 +184,11 @@ namespace UnicornGame
         {
             int SaveFileAmount = DirAccess.GetFilesAt(_directoryPath).Length;
             string[] TempArray = new string[SaveFileAmount];
-            string[] TimeStampArray = new string[SaveFileAmount];
+            //string[] TimeStampArray = new string[SaveFileAmount];
             TempArray = DirAccess.GetFilesAt(_directoryPath);
             for (int i = 0; i < _buttonList.Count; i++)
             {
+                GD.Print($"SetButtonVisibilityAndText for loop, i: {i}");
                 if (i > SaveFileAmount - 1)
                 {
                     //_buttonList[i].Disabled = true;
@@ -199,9 +200,13 @@ namespace UnicornGame
                 else
                 {
                     string SaveFilePath = GetSaveFilePath(TempArray[i]);
+                    //GD.Print($"SaveFilePath: {SaveFilePath}");
                     var GameDataDict = GetSaveFileAsDictionary(SaveFilePath);
-                    TimeStampArray = GetKeysArrayFromSave(SaveFileAmount, "TimeStamp", GameDataDict);
-                    _buttonList[i].Text = $"{TempArray[i]} {TimeStampArray[i]}";
+                    //GD.Print($"GameDataDict: {GameDataDict}");
+                    string TimeStamp = GetLatestTimeStampFromSave(GameDataDict);
+                    //GD.Print($"SetButtonVisibilityAndText() TimeStamp: {TimeStamp}");
+                    _buttonList[i].Text = $"{TempArray[i]} {TimeStamp}";
+                    //GD.Print($"_buttonList[i].Text: {_buttonList[i].Text}, TempArray[i]: {TempArray[i]}, TimeStamp: {TimeStamp} ");
                 }
             }
         }
@@ -235,9 +240,9 @@ namespace UnicornGame
             return LevelPathArray;
         }
         //Returns an array of the specified key DataKey's values on the second layer of the dictionary specified.
-        public string[] GetKeysArrayFromSave(int SaveFileAmount, string DataKey, Godot.Collections.Dictionary<string, Variant> GameDataDict)
+        public string[] GetKeysArrayFromSave(string DataKey, Godot.Collections.Dictionary<string, Variant> GameDataDict)
         {
-            string[] KeysArray = new string[SaveFileAmount];
+            string[] KeysArray = new string[GameDataDict.Keys.Count];
             int index = 0;
             foreach (var Key in GameDataDict.Keys)
             {
@@ -249,6 +254,26 @@ namespace UnicornGame
                 }
             }
             return KeysArray;
+        }
+        public string GetLatestTimeStampFromSave(Godot.Collections.Dictionary<string, Variant> GameDataDict)
+        {
+            string TimeStamp = "";
+            long UnixTime = 0;
+            foreach (var Key in GameDataDict.Keys)
+            {
+                GD.Print($"GetTimeStampFromSave() Key: {Key}");
+                var LevelDataDict = (Godot.Collections.Dictionary<string, Variant>)GameDataDict[Key];
+                if (LevelDataDict.ContainsKey("UnixTime"))
+                {
+                    if ((long)LevelDataDict["UnixTime"] > UnixTime)
+                    {
+                        UnixTime = (long)LevelDataDict["UnixTime"];
+                        TimeStamp = (string)LevelDataDict["TimeStamp"];
+                        GD.Print($"UnixTime: {UnixTime}, TimeStamp: {TimeStamp}");
+                    }
+                }
+            }
+            return TimeStamp;
         }
         //Checks the save file found in SaveFilePath for the last completed level and instantiates the next uncompleted level
         public void ChangeSceneToNextLevel(Godot.Collections.Dictionary<string, Variant> GameData, string SaveFilePath)
