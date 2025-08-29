298efbc55c41881acaa15f206f1c7394283e7211
Henri Hollanti <henri.hollanti@tuni.fi>
2025-08-29 21:43:13 +0300

    Progress with the prompts for deleting a save and a confirmation for deleting a particular save file

---
+++ MainMenu/Scripts/Prompt.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+namespace UnicornGame
+{
+    public partial class Prompt : Node2D
+    {
+        [Export] private Button _okButton;
+        [Export] private Button _cancelButton;
+
+        public override void _Ready()
+        {
+            
+        }
+    }
+}
+
